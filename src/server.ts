9bc76a56816931c2b94710765d16cc5c7fe474ca
Reuben Pasquini <reuben@frickjack.com>
2017-04-22 22:31:11 -0500

    Basic expressjs server running

---
+++ src/server.ts
@@ -0,0 +1,38 @@
+import express = require( 'express' );
+import bodyParser = require( 'body-parser' );
+import * as api from "./ApiService";
+
+const app = express();
+const service = api.ServiceFactory.getService();
+
+//app.use( express.static( __dirname ) );
+
+app.post( "/records", bodyParser.json(), function(req,res) {
+    const rec = api.RecordFactory.objectToRecord( req.body );
+    if ( ! rec ) {
+        res.status( 400 );
+        res.setHeader( "Content-Type", "text/plain" );
+        res.write( "Invalid record" );
+        res.end();
+        return;
+    }
+    service.addRecord( rec );
+});
+
+app.get( "/records/:orderBy", function(req,res) {
+    const orderBy = (function() {
+        switch (req.params.orderBy) {
+            case "name": return  "lastName";
+            case "birthdate" : return "dateOfBirth";
+            default: return req.params.orderBy;
+        }
+    })();
+    res.json( service.fetchAllRecords( orderBy ) );     
+});
+
+const port = 2500;
+app.listen( port, function() {
+    console.log( "Server listening at http://localhost:" + port + "/" );
+    console.log( "POST to /records" );
+    console.log( "GET from /records/:orderBy" );
+});
